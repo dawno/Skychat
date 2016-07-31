146066d2c0f7c306bd8961c0f5c9604cc6ce3944
dawno <utkarshsinghcs@gmail.com>
2016-07-31 13:22:03 +0530

    Updated Android studio

--- app/src/main/java/com/example/dellpc/skychat/Chatting.java
+++ app/src/main/java/com/example/dellpc/skychat/Chatting.java
@@ -9,12 +9,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import com.android.volley.Request;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,7 +38,9 @@ public class Chatting extends AppCompatActivity {
     Message pack;
     String fromName;
     String message;
+    private ListView messageView;
     private List<Message> packList = new ArrayList<>();
+    public static final String MyPREFERENCES = "" ;
     MessagesListAdapte adapter;
     String isSelf;
     private static final String TAG = Chatting.class.getSimpleName();
@@ -43,7 +51,9 @@ public class Chatting extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatting);
         final String receiver = getIntent().getStringExtra("name");
-
+        messageView = (ListView)findViewById(R.id.list_view_messages);
+        adapter = new MessagesListAdapte(this, packList);
+        messageView.setAdapter(adapter);
         mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar1);
         mActionBarToolbar.setTitle(receiver);
         setSupportActionBar(mActionBarToolbar);
@@ -51,38 +61,43 @@ public class Chatting extends AppCompatActivity {
         inputmsg = (EditText) findViewById(R.id.inputMsg);
         send = (Button) findViewById(R.id.btnSend);
 
-        SharedPreferences prefs = getSharedPreferences("bio", MODE_PRIVATE);
-        final String sender = prefs.getString("name", "");
+        SharedPreferences prefs = getSharedPreferences("Details", MODE_PRIVATE);
+        final String sender = prefs.getString("myName", "");
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final   String messageSent = inputmsg.getText().toString();
+                final String messageSent = inputmsg.getText().toString();
 
-                inputmsg.setText("");
-                messagesending(messageSent,receiver,sender);
+             inputmsg.setText("");
+
+                messagesending(messageSent, receiver, sender);
 
 
             }
         });
     }
 
-    private void messagesending( final String messagesent , final String receiver,final  String sender) {
+    private void messagesending( final String messageSent , final String receiver,final  String sender) {
 
 
-    JsonArrayRequest movieReq = new JsonArrayRequest(url,
-            new Response.Listener<JSONArray>() {
+   StringRequest movieRe = new StringRequest(Request.Method.POST,url,
+            new Response.Listener<String>() {
                 @Override
-                public void onResponse(JSONArray response) {
+                public void onResponse(String response) {
                     Log.d(TAG, response.toString());
 
 
+                    try {
+                    JSONArray Json = new JSONArray(response);
+                        Log.d("message mila ki ni"," mil gaya");
                     // Parsing json
-                    for (int i = 0; i < response.length(); i++) {
+                    for (int i = 0; i < Json.length(); i++) {
                         try {
 
-                            JSONObject obj = response.getJSONObject(i);
+                            JSONObject obj = Json.getJSONObject(i);
                             pack = new Message(fromName, message, isSelf);
-
+                            Log.d("message mila ki ni"," mil gaya");
+                            pack.setFromName(sender);
                             pack.setMessage(obj.getString("message"));
                             pack.setSelf(obj.getString("bool"));
 
@@ -100,11 +115,18 @@ public class Chatting extends AppCompatActivity {
                     // so that it renders the list view with updated data
                     adapter.notifyDataSetChanged();
                 }
+                    catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
             }, new Response.ErrorListener() {
         @Override
         public void onErrorResponse(VolleyError error) {
             VolleyLog.d(TAG, "Error: " + error.getMessage());
-
+            Log.e(TAG, "message loading  " + error.getMessage());
+            Toast.makeText(getApplicationContext(),
+                    error.getMessage(), Toast.LENGTH_LONG).show();
 
         }
     }) {
@@ -112,18 +134,20 @@ public class Chatting extends AppCompatActivity {
 
         @Override
         protected Map<String, String> getParams() {
-            // Posting parameters to login url
-            Map<String, String> params = new HashMap<String, String>();
-            params.put("sender_name",sender );
+            Map<String, String> params = new HashMap<>();
+            params.put("sender_name", sender);
+
             params.put("receiver_name", receiver);
-            params.put("message", messagesent);
+
+            params.put("message", messageSent);
+
             return params;
         }
 
     };
 
-        // Adding request to request queue
-        AppController.getInstance().addToRequestQueue(movieReq);
+
+        AppController.getInstance().addToRequestQueue(movieRe);
     }
 
 
